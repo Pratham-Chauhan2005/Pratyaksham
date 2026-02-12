4be18324f5d080be9a378fb78b2e2bfb869c5fcb
Pratham Chauhan <chauhanpratham741@gmail.com>
2026-02-12 22:25:45 +0530

    Deploying Frontend

--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -11,8 +11,9 @@ import StopScreenShareIcon from '@mui/icons-material/StopScreenShare'
 import ChatIcon from '@mui/icons-material/Chat'
 import Button from '@mui/material/Button';
 import { io } from "socket.io-client";
+import server from '../environment';
 
-const server_url = "http://localhost:8000"
+const server_url = server
 
 var connections = {};
 
